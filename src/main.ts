f4f3a9f59883987f41e56feb4af69ed521453434
aarifkhamdi <aarifkhamdi@gmail.com>
2024-03-15 11:27:45 +0500

    bug

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
-console.log('Hello World');
+import { createPublicClient, http } from 'viem';
+import { optimism } from 'viem/chains';
+
+(async () => {
+  const client = createPublicClient({
+    chain: optimism,
+    transport: http(),
+  });
+
+  const blockNumber = await client.getBlockNumber();
+
+  console.log(`Block number: ${blockNumber}`);
+})();
